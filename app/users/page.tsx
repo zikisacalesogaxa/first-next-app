ec8adebd701dc91ce2ce05b89c0629b763714fc0
zikisa <zikisacalesogaxa@gmail.com>
2025-08-12 23:11:32 +0200

    :: first next app

---
+++ app/users/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table className="table table-ordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default UsersPage;

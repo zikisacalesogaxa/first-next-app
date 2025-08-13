d26f2ae09e7abb90cfb9a9b8e45b4665d620a354
zikisa <zikisacalesogaxa@gmail.com>
2025-08-13 23:45:40 +0200

    :: add loading page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,17 +8,19 @@ interface User {
 }
 
 const UsersPage = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay
+
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
 
   return (
-    <>
-      <h1>Users</h1>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="font-bold text-5xl">Users</h1>
       <table className="table table-ordered">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Email</th>
+            <th className="font-bold text-xl">Name</th>
+            <th className="font-bold text-xl">Email</th>
           </tr>
         </thead>
         <tbody>
@@ -30,7 +32,7 @@ const UsersPage = async () => {
           ))}
         </tbody>
       </table>
-    </>
+    </div>
   );
 };
 
